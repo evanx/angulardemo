f8ac9fa7352ea63a08d3f09d94853bca85a5197d
Evan <evan.summers@gmail.com>
2014-02-08 00:59:55 +0200

    Create 2_app.js

---
+++ 2_app.js
@@ -0,0 +1,18 @@
+var app = angular.module("app", ['']);
+
+app.factory("appService", ["$http", function($http) {
+  return {
+    login: function(email, password) {
+    }    
+  }
+}]);
+
+app.controller("appController", ["$scope", "appService",
+  function($scope, appService) {
+    $scope.email = null;
+    $scope.login = function() {
+      appService.login();
+    };
+  }
+}]);
+
