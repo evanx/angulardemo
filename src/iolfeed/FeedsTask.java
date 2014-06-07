f305f6bbb2d9e0870ae9acb13063a59180eb803f
Evan Summers <evan.summers@gmail.com>
2014-06-07 13:31:24 +0200

    save future of the submitted task and next run() check if done and if execution exception occurred previous submission before resubmitting

--- src/iolfeed/FeedsTask.java
+++ src/iolfeed/FeedsTask.java
@@ -1,10 +1,15 @@
 package iolfeed;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import storage.ContentStorage;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,12 +20,13 @@ import org.slf4j.LoggerFactory;
 public class FeedsTask implements Runnable {
 
     static Logger logger = LoggerFactory.getLogger(FeedsTask.class);
-    
+
     ScheduledExecutorService elapsedExecutorService = Executors.newSingleThreadScheduledExecutor();
     ExecutorService taskExecutorService;
     FeedsContext context;
     ContentStorage storage;
-    
+    Map<String, Future> futureMap = new HashMap();
+
     public void start(FeedsContext context) throws Exception {
         this.context = context;
         taskExecutorService = Executors.newFixedThreadPool(context.feedTaskThreadPoolSize);
@@ -29,38 +35,44 @@ public class FeedsTask implements Runnable {
         } else {
             elapsedExecutorService.scheduleAtFixedRate(topTask, context.topInitialDelay,
                     context.topPeriod, TimeUnit.MILLISECONDS);
-            elapsedExecutorService.scheduleAtFixedRate(this, context.initialDelay, 
+            elapsedExecutorService.scheduleAtFixedRate(this, context.initialDelay,
                     context.period, TimeUnit.MILLISECONDS);
         }
     }
-    
+
     @Override
     public void run() {
         for (FeedEntity entity : context.feedEntityList) {
-            try {
-                if (!entity.getId().equals("top")) {
-                    submit(entity.getId());
-                }
-            } catch (Exception e) {
-                logger.warn("run: " + entity, e);
+            if (!entity.getId().equals("top")) {
+                submit(entity.getId());
             }
         }
     }
-    
+
     private final Runnable topTask = new Runnable() {
         final String section = "top";
-        
+
         @Override
         public void run() {
-            try {
-                submit(section);
-            } catch (Exception e) {
-                logger.warn("run: " + section, e);
-            }
+            submit(section);
         }
     };
-            
-    private void submit(String section) throws Exception {
-        taskExecutorService.submit(new FeedTask(context, section));
+
+    private void submit(String section) {
+        Future future = futureMap.get(section);
+        if (!future.isDone()) {
+            logger.warn("not done {}", section);
+        } else {
+            try {
+                future.get();
+            } catch (InterruptedException e) {
+                logger.warn("interrupted {}", section);
+            } catch (ExecutionException e) {
+                logger.error("exception {}", e.getCause());
+            }
+            futureMap.put(section,
+                    taskExecutorService.submit(
+                            new FeedTask(context, section)));
+        }
     }
 }
